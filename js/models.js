da1d3ec6ccd111effcd4ffabe925a6ac0ca534d5
Ap0c <Ap0c@users.noreply.github.com>
2015-10-14 21:48:17 +0100

    Added placeholder export functions for models module.

---
+++ js/models.js
@@ -0,0 +1,27 @@
+// ----- Exports ----- //
+
+exports.getNext = function getNext () {
+
+	// Returns next song in up next playlist, or null. Also fires 'new-playing'
+	// event.
+
+};
+
+exports.getPrev = function getPrev () {
+
+	// Returns previous song in up next playlist, or null. Also fires
+	// 'new-playing' event.
+
+};
+
+exports.upNext = function upNext () {
+
+	// Returns a list of song in the up next playlist.
+
+};
+
+exports.nowPlaying = function nowPlaying () {
+
+	// Returns the currently playing song.
+
+};
