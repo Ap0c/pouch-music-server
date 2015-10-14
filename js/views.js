76db94fd215a6017ff9eb289928875a5ab97bcf9
Ap0c <Ap0c@users.noreply.github.com>
2015-10-14 21:44:13 +0100

    Added placeholder export functions for views module.

---
+++ js/views.js
@@ -0,0 +1,37 @@
+// ----- Exports ----- //
+
+exports.navList = function navList (listItems) {
+
+	// Display listItems in list view.
+
+};
+
+exports.navArtist = function navArtist (artist) {
+
+	// Display artist view.
+
+};
+
+exports.navAlbum = function navAlbum (album) {
+
+	// Display album view.
+
+};
+
+exports.playerOverlay = function playerOverlay () {
+
+	// Bring up player overlay.
+
+};
+
+exports.menuOverlay = function menuOverlay () {
+
+	// Bring up menu overlay.
+
+};
+
+exports.updateNowPlaying = function updateNowPlaying (song, upNext) {
+
+	// Update song information in now playing bar and player overlay.
+
+};
